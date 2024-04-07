8f683c6126f989648d5d070261140f6bd4139056
Mohammadreza Norouzi <rda0044@gmail.com>
2024-04-07 21:05:45 +0330

    delete and edit post in admin panel

--- activities/Admin/Banner.php
+++ activities/Admin/Banner.php
@@ -21,4 +21,38 @@ class Banner extends Admin{
             } 
             $this->redirect('admin/banner');
         }
+        function edit($id)
+    {
+        
+        $db = new Database;
+        $banner = $db->select('SELECT * FROM banners WHERE id=?', [$id])->fetch();
+        require_once((BASE_PATH) . '/template/admin/banner/edit.php');
+    }
+        function update($request, $id){
+            $db = new Database;
+        
+            if ($request['image']['tmp_name'] != null) {
+                
+                $banner = $db->select('SELECT * FROM banners WHERE id=?', [$id])->fetch();
+                $this->removeImage($banner['image']);
+                $request['image'] = $this->saveImage($request['image'], 'banner-image');
+            } else {
+                unset($request['image']);
+            }
+            
+            $db->update('banners', $id, array_keys($request), array_values($request));
+        
+        $this->redirect('admin/banner');
+        }
+        function delete($id)
+    {   
+        
+        $db = new Database;
+        $banner = $db->select('SELECT * FROM banners WHERE id = ?', [$id])->fetch();
+
+        $this->removeImage($banner['image']);
+
+        $db->delete('banners', $id);
+        $this->redirect('admin/banner');
+    }
 }
--- template/admin/banner/index.php
+++ template/admin/banner/index.php
@@ -25,8 +25,8 @@ require_once(BASE_PATH . '/template/admin/layout/header.php');
                     <td><?= $banner['url'] ?></td>
                     <td><img style="width: 80px;" src="<?= asset($banner['image']) ?>" alt=""></td>
                     <td>
-                        <a role="button" class="btn btn-sm btn-primary text-white" href="">edit</a>
-                        <a role="button" class="btn btn-sm btn-danger text-white" href="">delete</a>
+                        <a role="button" class="btn btn-sm btn-primary text-white" href="<?= url('admin/banner/edit'.'/'.$banner['id']); ?>">edit</a>
+                        <a role="button" class="btn btn-sm btn-danger text-white" href="<?= url('admin/banner/delete'.'/'.$banner['id']); ?>">delete</a>
                     </td>
                 </tr>
 <?php } ?>
